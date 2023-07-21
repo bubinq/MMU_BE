845975f1a003c778466ed0b159aff06654a93079
moldovexc <alexanderparpulansky@gmail.com>
2023-07-21 20:41:31 +0300

    City Entity added

---
+++ src/main/java/team/yellow/docconnect/entity/City.java
@@ -0,0 +1,23 @@
+package team.yellow.docconnect.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "city")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class City {
+    @Id
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+}
