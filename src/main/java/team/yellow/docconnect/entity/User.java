65e2ec3e49f5a1783408d10b198141ee11ee089b
Daniel Baykov <danibaikov@gmail.com>
2023-07-22 17:37:39 +0300

    user entity added

---
+++ src/main/java/team/yellow/docconnect/entity/User.java
@@ -0,0 +1,29 @@
+package team.yellow.docconnect.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String first_name;
+
+    @Column(nullable = false)
+    private String last_name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private boolean verified;
+}
