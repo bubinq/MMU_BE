98a1962083cf1de5913426969ce82bd8828e17a8
Daniel Baykov <danibaikov@gmail.com>
2023-07-25 20:55:27 +0300

    implementing Spring Doc for City and Country Controllers

--- src/main/java/team/yellow/docconnect/SpringBootHealthcareAppApplication.java
+++ src/main/java/team/yellow/docconnect/SpringBootHealthcareAppApplication.java
@@ -1,8 +1,26 @@
 package team.yellow.docconnect;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+		title = "Spring Boot Library App REST APIs",
+		description = "Spring Boot Library App REST APIs Documentation",
+		version = "v1.0",
+		contact = @Contact(
+				name = "Ruben, Alexandr, Daniel",
+				email = "triple@gmail.com"
+		)
+),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Library App Documentation",
+				url = "https://github.com/aparpEdu/Library-App-REST-API"
+		)
+)
 @SpringBootApplication
 public class SpringBootHealthcareAppApplication {
 
