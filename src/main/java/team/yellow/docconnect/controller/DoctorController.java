e631e73300ec9f33e8737014005720d0238ed8d9
Daniel Baykov <danibaikov@gmail.com>
2023-07-25 21:03:28 +0300

    implementing Spring Doc for Doctor and Doctor Specialty Controllers

--- src/main/java/team/yellow/docconnect/controller/DoctorController.java
+++ src/main/java/team/yellow/docconnect/controller/DoctorController.java
@@ -1,5 +1,8 @@
 package team.yellow.docconnect.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import team.yellow.docconnect.utils.AppConstants;
 
 @RestController
 @RequestMapping("api/v1")
+@Tag(name = "CRUD REST APIs for Doctor Resource")
 public class DoctorController {
 
     private final DoctorService doctorService;
@@ -19,16 +23,40 @@ public class DoctorController {
         this.doctorService = doctorService;
     }
 
+    @Operation(
+            summary = "Create Doctor REST API",
+            description = "Create Doctor REST API is used to save doctor into database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
     @PostMapping("cities/{cityId}/doctors")
     public ResponseEntity<DoctorDto> createDoctor(@RequestBody @Valid DoctorDto doctorDto, @PathVariable Long cityId){
        return new ResponseEntity<>(doctorService.createDoctor(doctorDto, cityId), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Doctor By Id REST API",
+            description = "Get Doctor By Id REST API is used to get a single doctor from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @GetMapping("doctors/{doctorId}")
     public ResponseEntity<DoctorDto> getDoctorById(@PathVariable Long doctorId){
         return ResponseEntity.ok(doctorService.getDoctorById(doctorId));
     }
 
+    @Operation(
+            summary = "Get All Doctors REST API",
+            description = "Get All Doctors REST API is used to fetch all the doctors from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @GetMapping("doctors")
     public ResponseEntity<DoctorResponse> getAllDoctors
             (
@@ -40,6 +68,14 @@ public class DoctorController {
         return ResponseEntity.ok(doctorService.getAllDoctors(pageNo, pageSize, sortBy, sortDir));
     }
 
+    @Operation(
+            summary = "Get All Doctors By City Id REST API",
+            description = "Get All Doctors By City Id REST API is used to fetch all the doctors from the database by city id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @GetMapping("cities/{cityId}/doctors")
     public ResponseEntity<DoctorResponse> getAllDoctorsByCity
             (
@@ -52,11 +88,27 @@ public class DoctorController {
         return ResponseEntity.ok(doctorService.getAllDoctorsByCityId(cityId, pageNo, pageSize, sortBy, sortDir));
     }
 
+    @Operation(
+            summary = "Update Doctor REST API",
+            description = "Update Doctor REST API is used to update a particular doctor in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @PutMapping("doctors/{doctorId}")
     public ResponseEntity<DoctorDto> updateDoctorById(@PathVariable Long doctorId, @RequestBody @Valid DoctorDto doctorDto){
         return ResponseEntity.ok(doctorService.updateDoctorById(doctorId, doctorDto));
     }
 
+    @Operation(
+            summary = "Delete Doctor REST API",
+            description = "Delete Doctor REST API is used to delete doctor from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @DeleteMapping("doctors/{doctorId}")
     public ResponseEntity<String> deleteDoctorById(@PathVariable Long doctorId){
         doctorService.deleteDoctorById(doctorId);
