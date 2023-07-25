6df8e71f0fd80045af72e35934ce1a9391b895d1
ruben-izembrandt <ruben.izembrandt@university.mentormate.com>
2023-07-25 10:58:14 -0400

    Fixed dto response in DoctorSpecialtyMapper

--- src/main/java/team/yellow/docconnect/payload/mapper/DoctorSpecialtyMapper.java
+++ src/main/java/team/yellow/docconnect/payload/mapper/DoctorSpecialtyMapper.java
@@ -2,16 +2,17 @@ package team.yellow.docconnect.payload.mapper;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import team.yellow.docconnect.entity.DoctorSpecialty;
 import team.yellow.docconnect.payload.dto.DoctorSpecialtyDto;
-
 import java.util.List;
 
 @Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface DoctorSpecialtyMapper {
     DoctorSpecialtyMapper INSTANCE = Mappers.getMapper(DoctorSpecialtyMapper.class);
-
+    @Mapping(target = "doctorId", expression = "java(doctorSpecialty.getDoctor().getId())")
+    @Mapping(target = "specialtyId", expression = "java(doctorSpecialty.getSpecialty().getId())")
     DoctorSpecialtyDto entityToDTO(DoctorSpecialty doctorSpecialty);
 
     List<DoctorSpecialtyDto> entityToDTO(Iterable<DoctorSpecialty> cities);
