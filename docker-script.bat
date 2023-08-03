b95106885e36ae2bcb44e451aac695e5e569ac5f
moldovexc <alexanderparpulansky@gmail.com>
2023-08-03 18:27:59 +0300

    Refactoring DockerFile to support maven

--- docker-script.bat
+++ docker-script.bat
@@ -1,8 +1,5 @@
 @echo off
 
-rem Change to the directory where your docker-compose.yml file is located
-cd /d "C:\Users\ShittyRestaurant\eclipse-workspace\back-ends"
-
 rem Bring down any existing containers and networks
 docker-compose down
 
