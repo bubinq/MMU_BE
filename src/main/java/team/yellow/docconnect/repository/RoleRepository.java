070af235e203ba4ec41cb5d1e8d29176fe82aba4
Daniel Baykov <danibaikov@gmail.com>
2023-08-05 18:57:34 +0300

    adding role and user repository

---
+++ src/main/java/team/yellow/docconnect/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package team.yellow.docconnect.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.yellow.docconnect.entity.Role;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+}
--- src/main/java/team/yellow/docconnect/repository/UserRepository.java
+++ src/main/java/team/yellow/docconnect/repository/UserRepository.java
@@ -3,5 +3,12 @@ package team.yellow.docconnect.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.yellow.docconnect.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmailIgnoreCase(String email);
+
+    Boolean existsByEmailIgnoreCase(String email);
+
 }
