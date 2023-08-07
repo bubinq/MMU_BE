87ee8c0a8167192865b621ee6bd1509b7aad178a
Daniel Baykov <danibaikov@gmail.com>
2023-08-07 17:42:26 +0300

    refactoring register validation

--- src/main/java/team/yellow/docconnect/payload/dto/RegisterDto.java
+++ src/main/java/team/yellow/docconnect/payload/dto/RegisterDto.java
@@ -16,22 +16,22 @@ import team.yellow.docconnect.validator.ValidPassword;
 public record RegisterDto(
 
         @NotEmpty(message = "Please enter a first name")
-        @Size(min = 2, max = 50, message = "First name must be less than 50 characters long")
+        @Size(min = 1, max = 49, message = "First name must be less than 50 characters long")
         String firstName,
 
         @NotEmpty(message = "Please enter a last name")
-        @Size(min = 2, max = 50, message = "Last name must be less than 50 characters long")
+        @Size(min = 1, max = 49, message = "Last name must be less than 50 characters long")
         String lastName,
 
         @NotEmpty(message = "Please enter a password")
         @Size(min = 8, message = "Password must be at least 8 characters long")
-        @Size(max = 100, message = "Password must be less than 100 characters long")
+        @Size(max = 99, message = "Password must be less than 100 characters long")
         @ValidPassword
         String password,
 
         @NotEmpty(message = "Please enter a matching password")
         @Size(min = 8, message = "Matching Password should be between 8 and 20 characters")
-        @Size(max = 100, message = "Matching Password must be less than 100 characters long")
+        @Size(max = 99, message = "Matching Password must be less than 100 characters long")
         @ValidPassword
         String matchingPassword,
 
