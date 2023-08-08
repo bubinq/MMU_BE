23962768fbe165021cf80243a46b8779d60f058a
Daniel Baykov <danibaikov@gmail.com>
2023-08-08 16:17:46 +0300

    refactoring oauth2 decoding in security config

--- src/main/java/team/yellow/docconnect/config/SecurityConfig.java
+++ src/main/java/team/yellow/docconnect/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package team.yellow.docconnect.config;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,15 +20,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import team.yellow.docconnect.security.JwtAuthenticationEntryPoint;
 import team.yellow.docconnect.security.JwtAuthenticationFilter;
+import team.yellow.docconnect.security.JwtTokenProvider;
 
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
+import java.security.Key;
+import java.util.Date;
 
 @Configuration
 @EnableWebSecurity
@@ -41,15 +45,16 @@ public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    @Value("${jwt.public.key}")
-    RSAPublicKey key;
-    @Value("${jwt.private.key}")
-    RSAPrivateKey privateKey;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public SecurityConfig(UserDetailsService userDetailsService, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAuthenticationFilter jwtAuthenticationFilter) {
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    public SecurityConfig(UserDetailsService userDetailsService, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAuthenticationFilter jwtAuthenticationFilter, JwtTokenProvider jwtTokenProvider) {
         this.userDetailsService = userDetailsService;
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Bean
@@ -76,24 +81,58 @@ public class SecurityConfig {
                                 .requestMatchers("/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling((exceptions) ->
                         exceptions.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .oauth2Login(oauth2Login ->
-                        oauth2Login.loginPage("/api/v1/auth/google_login"));
-//                .oauth2ResourceServer(
-//                        httpSecurityOAuth2ResourceServerConfigurer -> httpSecurityOAuth2ResourceServerConfigurer
-//                                .jwt(
-//                                        jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())));
+                        oauth2Login.loginPage("/api/v1/auth/google_login"))
+                .oauth2ResourceServer(
+                        httpSecurityOAuth2ResourceServerConfigurer -> httpSecurityOAuth2ResourceServerConfigurer
+                                .jwt(
+                                        jwtConfigurer -> jwtConfigurer.decoder(customJwtDecoder())))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
 
     }
 
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder.withPublicKey(this.key).build();
+//    }
+
     @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withPublicKey(this.key).build();
+    public JwtDecoder customJwtDecoder() {
+        // Use your JwtTokenProvider for token validation
+        return new JwtDecoder() {
+            @Override
+            public Jwt decode(String token) {
+                if (jwtTokenProvider.validateToken(token)) {
+                    Claims claims = Jwts.parserBuilder()
+                            .setSigningKeyResolver(signingKeyResolver()) // Use your key resolver
+                            .build()
+                            .parseClaimsJws(token)
+                            .getBody();
+
+                    Date issuedAt = claims.getIssuedAt();
+                    Date expiresAt = claims.getExpiration();
+
+                    return new Jwt(token, issuedAt.toInstant(), expiresAt.toInstant(), claims, claims);
+                } else {
+                    throw new RuntimeException("Invalid JWT token");
+                }
+            }
+        };
+    }
+
+    @Bean
+    public SigningKeyResolver signingKeyResolver() {
+        return new SigningKeyResolverAdapter() {
+            @Override
+            public Key resolveSigningKey(JwsHeader header, Claims claims) {
+                return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+            }
+        };
     }
 }
