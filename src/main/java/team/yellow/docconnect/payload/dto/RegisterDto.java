79f9896c9bade250e6a3542bd7dd5d9161151bae
Daniel Baykov <danibaikov@gmail.com>
2023-08-10 13:39:38 +0300

    fixing RegisterDto validations

--- src/main/java/team/yellow/docconnect/payload/dto/RegisterDto.java
+++ src/main/java/team/yellow/docconnect/payload/dto/RegisterDto.java
@@ -16,22 +16,22 @@ import team.yellow.docconnect.validator.ValidPassword;
 public record RegisterDto(
 
         @NotEmpty(message = "Please enter a first name")
-        @Size(min = 1, max = 49, message = "First name must be less than 50 characters long")
+        @Size(min = 1, max = 50, message = "First name must not exceed 50 characters")
         String firstName,
 
         @NotEmpty(message = "Please enter a last name")
-        @Size(min = 1, max = 49, message = "Last name must be less than 50 characters long")
+        @Size(min = 1, max = 50, message = "Last name must not exceed 50 characters")
         String lastName,
 
         @NotEmpty(message = "Please enter a password")
         @Size(min = 8, message = "Password must be at least 8 characters long")
-        @Size(max = 99, message = "Password must be less than 100 characters long")
+        @Size(max = 100, message = "Password must not exceed 100 characters")
         @ValidPassword
         String password,
 
         @NotEmpty(message = "Please enter a matching password")
-        @Size(min = 8, message = "Matching Password should be between 8 and 20 characters")
-        @Size(max = 99, message = "Matching Password must be less than 100 characters long")
+        @Size(min = 8, message = "Matching Password should be at least 8 characters long")
+        @Size(max = 100, message = "Matching Password must not exceed 100 characters")
         @ValidPassword
         String matchingPassword,
 
