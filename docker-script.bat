fbc62ebfad7ea90e8c1c5cc7954ca6fe47276f75
moldovexc <alexanderparpulansky@gmail.com>
2023-08-12 01:27:05 +0300

    Upgrading Docker Compose to V2

--- docker-script.bat
+++ docker-script.bat
@@ -1,11 +1,8 @@
 @echo off
 
 rem Bring down any existing containers and networks
-docker-compose down
-
-@REM rem Build the Docker images
-@REM docker-compose build
+docker compose down
 
 rem Start the containers in background mode
-docker-compose up -d --build
+docker compose up -d --build
 
