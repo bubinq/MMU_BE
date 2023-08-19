afe1521f0ea21d9d05434b5e603f626b94f3815f
Daniel Baykov <danibaikov@gmail.com>
2023-08-19 11:35:21 +0300

    fixing repository methods

--- src/main/java/team/yellow/docconnect/repository/UserRepository.java
+++ src/main/java/team/yellow/docconnect/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package team.yellow.docconnect.repository;
 
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,13 +14,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByEmailIgnoreCase(String email);
 
-    @Operation(
-            summary = "Update User's Confirm Status by Email",
-            description = "Update User's Confirm Status by Email is used to update user's confirm status by email"
-    )
     @Transactional
     @Modifying
     @Query("UPDATE User u " +
-            "SET u.isVerified = TRUE WHERE u.email = ?1")
+            "SET u.isEmailVerified = TRUE WHERE u.email = ?1")
     void confirmEmail(String email);
 }
